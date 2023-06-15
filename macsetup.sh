780cdab52fe406567be71bf16a717cf6ebc4087e
Roddur Dasgupta <roddurd@gmail.com>
2023-06-14 17:43:54 -0700

    Create macsetup.sh

---
+++ macsetup.sh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+
+# Get Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# brew install formulae
+brew install                 \
+  git                        \
+  koekeishiya/formulae/skhd  \
+  koekeishiya/formulae/yabai \
+  vim                        \
+  zsh-autosuggestions        \
+  zsh-syntax-highlighting
+
+# brew install apps
+brew install --cask  \
+  anki               \
+  arc                \
+  copyq              \
+  discord            \
+  docker             \
+  fl-studio          \
+  obs                \
+  rectangle          \
+  spotify            \
+  visual-studio-code \
+  warp
+  
+# ---- non homebrew stuff ------
+
+# Clone my dotfiles
+git clone https://github.com/roddurd/dotfiles.git ~/dotfiles
+
+# Create symbolic links between dotfiles and the home directory
+ln -s ~/dotfiles/.skhdrc ~/.skhdrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.yabairc ~/.yabairc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+
+source ~/.zshrc

754437fa8820f4dfec2f9077261705e32e770e7e
Roddur Dasgupta <roddurd@gmail.com>
2023-06-26 11:39:18 -0700

    move packer boostrap to packerinfo

--- .config/nvim/lua/packerinfo.lua
+++ .config/nvim/lua/packerinfo.lua
@@ -1,5 +1,20 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
+-- bootstrap packer
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- actual plugins
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
